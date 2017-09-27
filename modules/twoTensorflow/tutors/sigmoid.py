03ff3acb9b72879b0e791a4b081b4e33b2a96d96
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-09-27 09:50:11 +0200

    made a function to see how sigmoid function works

--- modules/twoTensorflow/tutors/sigmoid.py
+++ modules/twoTensorflow/tutors/sigmoid.py
@@ -1,1 +1,32 @@
-import mathplotlib
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+def sigmoid(a):
+    # e = math.exp() # e ≈ 2.7
+    e = 2.7
+    return (1 / (1 + e**(-a)))
+
+
+
+def showGraph():
+    sigmoidValues = []
+    for i in range(-10,10):
+        sigmoidValues.append(sigmoid(i))
+
+    print("Sigmoid values [-10,10]: " + str(sigmoidValues))
+
+    xAxis = np.arange(-10, 10) # make a list from (-10 to 10) with a step of one
+    yAxis = sigmoidValues
+
+
+    plt.ylabel("hmm...")
+    plt.xlabel("what are these numbers?")
+
+    plt.plot(xAxis, yAxis)
+    plt.show()
+
+
+
+showGraph()
+
