f78260ee3412b8057b3ffa8ab0c0194ebc7647a5
Woldt <thmwld@gmail.com>
2017-10-02 15:33:56 +0200

    added main

---
+++ modules/twoTensorflow/main.py
@@ -0,0 +1,18 @@
+from tutorialThree import Gann, autoex, Caseman
+
+def runModule():
+    dims = None #Layer-sizes
+    caseMan = None
+    lrate = None #Learning rate
+    showint  = None # show interval
+    mbs = None # mini batch size
+    vint = None # validation history
+    softmax = None # softmax_0utputs # this is a regression / learning algorthm thingy thang
+
+def test():
+
+    c = Caseman(cfunc="winequality_red.txt")
+
+
+
+test()
