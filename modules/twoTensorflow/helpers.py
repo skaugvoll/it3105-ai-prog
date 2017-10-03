4b139f64af236f7b96f19cc3776269b3742e7860
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-10-03 11:32:41 +0200

    fixed c and l

--- modules/twoTensorflow/helpers.py
+++ modules/twoTensorflow/helpers.py
@@ -1,19 +1,23 @@
 import os
-
 def converteDatasetTo2d(datapath):
     filepath = os.getcwd() + "/data/" + datapath
 
     cases2d = []
     with open(filepath) as FileObj:
         for caseline in FileObj:
-            caseline = caseline.rstrip()
+            caseline = caseline.rstrip() # remove newline character
 
-            c = caseline.split(";")
-            c = list(map(lambda x: float(x), c))
+            c = caseline.split(";") # make a list out of line
+            c = list(map(lambda x: float(x), c)) # make elements float
 
-            l = c[-1:]
-
+            l = c[-1:] # get label
+            c = c[:-1] # remove label
             case = [c,l]
+
+            # print("C: ", c)
+            # print("L: ", l)
+            # print("Case: ", case)
+
             cases2d.append(case)
 
     return cases2d
