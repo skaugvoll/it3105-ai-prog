f8aa716accf46a509cd28fd4fad4540b6618aa90
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-10-04 20:07:46 +0200

    now it runs, somewhat

--- modules/twoTensorflow/main.py
+++ modules/twoTensorflow/main.py
@@ -18,4 +18,32 @@ def test():
 
     autoex()
 
-test()
+# test()
+
+
+def gannTesting(epochs=300,nbits=4,lrate=0.03,showint=100,mbs=None,vfrac=0.1,tfrac=0.1,vint=100,sm=False):
+    # size = 2**nbits I autoex, så gir denne 16, som er så mange elementer i hver liste / features
+
+
+    mbs = mbs if mbs else 11
+
+    case_generator = (lambda : helpers.converteDatasetTo2d("winequality_red.txt"))
+
+    cman = Caseman(cfunc=case_generator,vfrac=vfrac,tfrac=tfrac)
+
+    # dims = [features, hidden layer,...,hidden layer n-1, hiden layer n, labels]
+    dims = [11, nbits, 1]
+
+    ann = Gann(dims=dims,cman=cman,lrate=lrate,showint=showint,mbs=mbs,vint=vint,softmax=sm)
+
+    ann.gen_probe(0,'wgt',('hist','avg'))  # Plot a histogram and avg of the incoming weights to module 0.
+    ann.gen_probe(1,'out',('avg','max'))  # Plot average and max value of module 1's output vector
+
+    ann.add_grabvar(0,'wgt') # Add a grabvar (to be displayed in its own matplotlib window).
+
+    ann.run(epochs)
+    ann.runmore(epochs*2)
+
+    return ann
+
+gannTesting()
