cbb6b5584cf61ed3819e33bcba73f6365d9be812
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-10-18 14:59:33 +0200

    tried filling in parameters to other datasets

--- modules/twoTensorflow/helpers.py
+++ modules/twoTensorflow/helpers.py
@@ -80,15 +80,26 @@ def add_prob_grabvars(ann, newGrabvars):
         ann.gen_probe(grabvar[0], grabvar[1], grabvar[2])
 
 
-def get_case_generator(data_name="autoencoder", nbits=4, numberOfClasses=None):
+def get_case_generator(
+        data_name="autoencoder",
+        numberOfClasses=None,
+        nbits=4,
+        size= None,
+        density = (0,1),
+        double=True,
+        random = True,
+        poptarg=True,
+        minsegs=1,
+        maxsegs=10
+    ):
     if data_name == 'yeast': return "(lambda: helpers.converteDatasetTo2d('yeast.txt', 10))"
     if data_name == 'glass': return "(lambda: helpers.converteDatasetTo2d('glass.txt', 7))"
     if data_name == 'wine': return "(lambda : helpers.converteDatasetTo2d('winequality_red.txt', 6))"
     if data_name == 'autoencoder': return "(lambda: TFT.gen_all_one_hot_cases(2 ** "+ str(nbits) +"))"
-    if data_name == 'parity': return "(lambda: TFT.gen_all_parity_cases())"
-    if data_name == 'dense': return "(lambda: TFT.gen_dense_autoencoder_cases())"
-    if data_name == 'bit': return "(lambda: TFT.gen_vector_count_cases())"
-    if data_name == 'segment': return "(lambda: TFT.gen_segment_vector_cases())"
+    if data_name == 'parity': return "(lambda: TFT.gen_all_parity_cases("+ str(nbits) +","+ str(double) +"))"
+    if data_name == 'dense': return "(lambda: TFT.gen_dense_autoencoder_cases("+ str(nbits) +","+ str(size) +","+ str(density) +"))"
+    if data_name == 'bit': return "(lambda: TFT.gen_vector_count_cases("+ str(nbits) +","+ str(size) +","+ str(density) +","+ str(random) +","+ str(poptarg) +"))"
+    if data_name == 'segment': return "(lambda: TFT.gen_segmented_vector_cases("+ str(nbits) +","+ str(size) +","+ str(minsegs) +","+ str(maxsegs) +","+ str(poptarg) +"))"
     # if data_name == 'mnist': return
     # if non of above, must have typed in another one
     return "(lambda: helpers.converteDatasetTo2d(data_name, "+ str(numberOfClasses) +"))"
