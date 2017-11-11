4db9974050c157775ff0fbf26a5b9f249a4a063d
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-11-11 22:01:17 +0100

    tried implementing neighbour updating

--- modules/threeSOM/TSP.py
+++ modules/threeSOM/TSP.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+from time import sleep
 import matplotlib.animation as animation
 
 
@@ -29,7 +30,7 @@ def readFile():
 # Generates a circle of neurons in the middle og the plot.
 def generateNeurons(points):
     neurons = []
-    slice = 2 * math.pi / (len(points) * 2);
+    slice =  2 * math.pi / (len(points) * 2)
     for i in range(len(points) * 2):
         angle = slice * i
         newX = 0.5 + (0.2 * math.cos(angle))
@@ -38,11 +39,11 @@ def generateNeurons(points):
 
     return neurons
 
-# For each iteration, this funktion plots input points and neurons into matplotlib
-def plotPoints(inputs, neurons):
+# For each iteration, this function plots input points and neurons into matplotlib
+def plotPoints(inputs, neurons, step=''):
     plt.clf()
     nx, ny = neurons.T
-    plt.scatter(nx, ny, color="red")
+    plt.scatter(nx, ny, color='red')
     plt.plot(nx, ny, color="black")
 
     px, py = inputs.T
@@ -50,11 +51,12 @@ def plotPoints(inputs, neurons):
     plt.xlim([-0.02, 1.02])
     plt.scatter(px, py)
 
+    plt.figtext(0.02, 0.02, "Step: " + str(step))
     plt.draw()
     plt.pause(0.00000003)
 
-# Finds the closest neuron with weuqlidian distance.
-def findAndUpdateWinnerNeuron(inputPoint, neurons):
+# Finds the closest neuron with euclidean distance.
+def findWinnerNeuron(inputPoint, neurons):
     winnerNeuronIndex = None
     lowestDist = math.inf
     for i in range(len(neurons)):
@@ -62,35 +64,81 @@ def findAndUpdateWinnerNeuron(inputPoint, neurons):
         if dist < lowestDist:
             lowestDist = dist
             winnerNeuronIndex = i
-
-    return updateNeuron(inputPoint, neurons, winnerNeuronIndex)
+    return winnerNeuronIndex
 
 # updates the closest neuron and moves it towards the input point.
-def updateNeuron(inputPoint, neurons, winnerIndex):
-    lr = 0.3
-    dirX = lr * (neurons[winnerIndex][0] - inputPoint[0])
-    dirY = lr * (neurons[winnerIndex][1] - inputPoint[1])
+def updateNeuron(inputPoint, neurons, winnerIndex, neighborhoodShip=None, lr=0.3, decay=1):
+    dirX = lr * (neurons[winnerIndex][0] - inputPoint[0]) * decay
+    dirY = lr * (neurons[winnerIndex][1] - inputPoint[1]) * decay
+    # dirX = neurons(winnerIndex)[0] + lr * neighborhoodShip * (inputPoint[0] - neurons[winnerIndex][0])
+    # dirY = neurons(winnerIndex)[1] + lr * neighborhoodShip * (inputPoint[1] - neurons[winnerIndex][1])
     neurons[winnerIndex][0] -= dirX
     neurons[winnerIndex][1] -= dirY
 
     return neurons
+
 
 def run():
     points = readFile()
     maxVal = np.amax(points)  # Find the biggest value in the array to use for scaling
     inputs = np.array(points) / maxVal  # Make np.array and scale the values (between 0 and 1)
 
-    neurons = generateNeurons(points)
+    neurons = generateNeurons(points * 2)
     neurons = np.array(neurons)
 
     plotPoints(inputs, neurons)
 
-    for i in range(200):
-        randInput = random.randint(0, len(points) - 1)
-        neurons = findAndUpdateWinnerNeuron(inputs[randInput], neurons)
-        plotPoints(inputs, neurons)
+    i = 1
+    viewInterval = 250
+    neighborhoodSize = 10
+    learningRate = 0.5
+    converged = False
+    while i < 5000 and not converged:
+        for c in inputs:
+            randInput = random.randint(0, len(points) - 1)
+            winnerIndex = findWinnerNeuron(inputs[randInput], neurons)
+            neurons = updateNeuron(inputs[randInput], neurons, winnerIndex, lr=learningRate)
 
-# TODO: oppdatere naboene til vinner-neuron og flytte disse mot input point som bli sjekket. Skal ikke flyttes like langt som vinner-neuron. Tror man kan regne ut naboer med euqlidian distance for å finne de to nærmeste
-# Litt usikker på hva mer som må gjøres. Sjekk link jeg sendte deg på messenger
+            # find neighbours
+            learningRate = 1 / i ** (1 / 4)
+
+            if i == 1:
+                neighborhoodSize = 10
+            else:
+                neighborhoodSize = max(int(neighborhoodSize * (1 - 0.01 * i)), 2)
+
+            neighborhood = []
+            for n in range(1, neighborhoodSize+1):
+                if winnerIndex == 0:
+                    neighborhood.append(len(neurons) - n)
+                elif winnerIndex == len(neurons) - n:
+                    neighborhood.append(winnerIndex - n)
+                    neighborhood.append(0)
+                else:
+                    neighborhood.append(winnerIndex - n)
+                    neighborhood.append(winnerIndex + n)
+
+
+            for neighborIndex in neighborhood:
+                # update their weights
+                neurons = updateNeuron(inputs[randInput], neurons, neighborIndex, decay=0.5, lr=learningRate)
+
+                if i % viewInterval == 0:
+                    plotPoints(inputs, neurons, step=i)
+
+        i += 1
+        # TODO: fix at den sjekker om vi har funent convergens
+
+    # TODO : Gå gjennom alle byer, og finn nærmeste neuron. Så må vi mappe det på en måte, slik at vi får neuron: by, Så gå gjennom neuronene i den ringrekkefølgen de ligger i, og sjekker om de har en by, isåfall legg til avstanden mellom dette nehronet og neste neuron
+    paths = {}
+    dist = 0
+    for city in inputs:
+        paths[str(city)] = findWinnerNeuron(city, neurons)
+        dist += distance.euclidean(city, paths[str(city)])
+
+    print('Path dist: {:.2f}km\nOptimal dist: {:.2f}km\n= {:.2f}%'.format(dist, 7542, (dist/7542)))
+    sleep(10)
+
+
 
 run()
