843a8c42ce5c61e27fed52e1e8c7450c72f5de38
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-11-13 16:42:17 +0100

    fuck this shit

--- modules/threeSOM/Classifier.py
+++ modules/threeSOM/Classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 from time import sleep
 from Neuron import Neuron
+from scipy.spatial import distance
 
 cwd = os.getcwd()
 sys.path.append(cwd + "/mnist/")
@@ -22,29 +23,32 @@ def generateNeurons(numberOfNeurons=100, pixler=784):
 
 
 
-def draw(root, neurons, dim=10):
+def draw(root, neurons, dim=100):
     can = Canvas(root, width=900, height=900)
     can.pack()
 
     # one neuron = one picture, one picture has 784 pixels / rectangles
-    # print(neuron)
-    # drawOneNeuron(can, neuron, 0, 0, "red")
-    # drawOneNeuron(can, neuron, 84, 0, "red")
-    # drawOneNeuron(can, neuron, 0, 0, "red")
-    # drawOneNeuron(can, neuron, 0, 0, "red")
+
     offsettRow = 0
     offsettCol = 0
+    split = np.floor(np.sqrt(dim))
+    i = 1
     for n in range(len(neurons)):
         neuron = neurons[n]
-        if n % dim == 0:
+        if i == split:
             offsettRow += 84
             offsettCol = 0
-        else:
+            i = 1
+        elif i != 1:
             offsettCol += 84
-        drawOneNeuron(can, neuron, offsettRow, offsettCol, "black")
+        drawOneNeuron(can, neuron, offsettRow, offsettCol)
+        root.update_idletasks()
+        root.update()
+
+        i += 1
 
 
-def drawOneNeuron(can, neuron, row, col, color):
+def drawOneNeuron(can, neuron, row, col):
     idx = 0
     row = row
     column = col
@@ -71,6 +75,12 @@ def findWinnerNeuron(case, neurons):
     return winnerNeuronIndex
 
 
+def getCoordinates(idx):
+    if idx < 10:
+        return [0, idx]
+    else:
+        num = str(idx)
+        return [int(num[:1]), int(num[1:])]
 
 
 def run():
@@ -79,8 +89,7 @@ def run():
 
     neurons = generateNeurons() # randomly initialize 100 neruons with 784 pixlers each.
     draw(gui, neurons)
-    gui.update()
-    sleep(5)
+
 
     maxEpoc = 100
     converged = False
@@ -99,18 +108,22 @@ def run():
         ### STEPS
         for case in range(len(data)):
             winnerNeuron = findWinnerNeuron(case, neurons)
+            winnerCoordinates = getCoordinates(winnerNeuron)
 
             ## UPDATE ALL NEURONS ? or update Winner and some Neighbours
             for idx in range(len(neurons)):
-                #### Wrapper for å finne korteste vei i sirkelen (type wraparound list)
-
-                #distance = distasen i grid og ikke bilder. så x og y kordinater i grid.
-                dist = None
-                ####
+                #distance = distansen i grid og ikke bilder. så x og y kordinater i grid.
+                neuronCoord = getCoordinates(idx)
+                dist = distance.euclidean(winnerCoordinates, neuronCoord)
 
                 neighborhoodMembership = np.exp(-dist ** 2 / neighborhoodSize ** 2)
-                neurons[idx][0] = np.add(neurons[idx][0], np.prod([learningRate,neighborhoodMembership,(inputs[c][0] - neurons[idx][0])]))
-                neurons[idx][1] = np.add(neurons[idx][1], np.prod([learningRate,neighborhoodMembership,(inputs[c][1] - neurons[idx][1])]))
+                neurons[idx] = np.add(neurons[idx], (learningRate * neighborhoodMembership * np.subtract(data[case], neurons[idx])))
+
+        if epoc % viewInterval == 0:
+            draw(gui, neurons)
+
+
+        epoc += 1
 
 
 
