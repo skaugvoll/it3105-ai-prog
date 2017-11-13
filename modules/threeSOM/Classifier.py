5937a9829335d4e2d1ce1d2b9608b278656138e8
Woldt <thmwld@gmail.com>
2017-11-13 10:19:17 +0100

    added classifier which finds 500 cases from mnist dataset

--- modules/threeSOM/Classifier.py
+++ modules/threeSOM/Classifier.py
@@ -1,1 +1,20 @@
 from tkinter import *
+import os
+import numpy as np
+
+cwd = os.getcwd()
+sys.path.append(cwd + "/mnist/")
+
+import mnist_basics as MNIST
+
+def run():
+    gui = Tk()
+
+
+
+def loadData():
+    ca = np.array(MNIST.load_all_flat_cases()[0])
+    np.random.shuffle(ca)
+    ca = ca[:500]
+
+loadData()
---
+++ modules/threeSOM/Neuron.py
@@ -0,0 +1,10 @@
+
+class Neuron:
+    def __init__(self, x=None, y=None, weights=None):
+        self.x = x,
+        self.y = y,
+        self.weights = weights,
+        self.nn = None
+        self.ns = None
+        self.ne = None
+        self.nw = None
