d7802ad549ebc967395c527e9a0457dbb726c7d9
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-11-14 12:48:50 +0100

    removed unused code

--- modules/threeSOM/Neuron.py
+++ modules/threeSOM/Neuron.py
@@ -7,7 +7,3 @@ class Neuron:
         self.weights = np.random.rand(1, numberOfPixels)[0]
         self.winnerlabels = np.zeros((1,numberOfClasses))[0] # index 0 = tallet 0, index 1 = tallet 1, etc.
         self.currentLabel = None
-        self.nn = None
-        self.ns = None
-        self.ne = None
-        self.nw = None
